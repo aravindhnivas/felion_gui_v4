9195d2ee0ec2e022818165f96e0db16031e0b592
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-12-03 11:18:12 +0100

    Update utils.ts

--- src/js/utils.ts
+++ src/js/utils.ts
@@ -1,20 +1,21 @@
-import {SpeedOfLight} from '$src/js/constants'
+import { SpeedOfLight } from '$src/js/constants'
+import { fs } from '@tauri-apps/api'
 import { parse as Yml } from 'yaml'
 
 export const wavenumberToMHz = (energy: ValueLabel<number>) => ({
     ...energy,
-    value: Number(energy.value * SpeedOfLight * 1e2 * 1e-6)
+    value: Number(energy.value * SpeedOfLight * 1e2 * 1e-6),
 })
 
 export const MHzToWavenumber = (energy: ValueLabel<number>) => ({
     ...energy,
-    value: Number(energy.value / (SpeedOfLight * 1e2 * 1e-6))
+    value: Number(energy.value / (SpeedOfLight * 1e2 * 1e-6)),
 })
 
 export const getYMLFileContents = async (filename: string) => {
-    if (!window.fs.isFile(filename)) return Promise.reject(filename + " file doesn't exist")
-    const fileContent = window.fs.readFileSync(filename)
-    if (window.fs.isError(fileContent)) return Promise.reject("Couldn't read file " + filename)
+    if (!(await fs.exists(filename))) return Promise.reject(filename + " file doesn't exist")
+    const fileContent = await fs.readTextFile(filename)
+    if (isError(fileContent)) return Promise.reject("Couldn't read file " + filename)
     const YMLcontent = Yml(fileContent)
     return Promise.resolve(YMLcontent)
 }
