d2df009d3bc4a9968fcfafc017d96e45f9df1ab1
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-12-03 21:41:13 +0100

    Create ModalTable.svelte

---
+++ src/components/modal/ModalTable.svelte
@@ -0,0 +1,30 @@
+<script>
+    import Modal from '$components/modal/Modal.svelte'
+    import Table from '$components/tables/Table.svelte'
+
+    export let rows = []
+    export let keys = []
+    export let title = ''
+    export let active = false
+    export let userSelect = true
+    export let animateRow = false
+    export let sortOption = false
+
+    export let closeOption = true
+    export let addextraOption = true
+</script>
+
+{#if active}
+    <Modal bind:active {title}>
+        <svelte:fragment slot="content">
+            <Table head={keys} {keys} bind:rows {sortOption} {closeOption} {addextraOption} {animateRow} {userSelect}>
+                <svelte:fragment slot="header">
+                    <slot name="header" />
+                </svelte:fragment>
+            </Table>
+        </svelte:fragment>
+        <svelte:fragment slot="footerbtn">
+            <slot name="footer"><button class="button is-link" on:click={() => (active = false)}>Close</button></slot>
+        </svelte:fragment>
+    </Modal>
+{/if}
