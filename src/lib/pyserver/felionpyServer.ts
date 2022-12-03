5c66344c27b6f35d821352f2765aac01e04e9fd6
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-12-03 17:57:15 +0100

    Update felionpyServer.ts

--- src/lib/pyserver/felionpyServer.ts
+++ src/lib/pyserver/felionpyServer.ts
@@ -1,19 +1,26 @@
-import { pyProgram, developerMode, pyServerReady, pyServerPORT, pythonscript, get } from '$lib/pyserver/stores'
+import {
+    pyProgram,
+    developerMode,
+    pyServerReady,
+    pyServerPORT,
+    pythonscript,
+    serverDebug,
+    get,
+} from '$lib/pyserver/stores'
 import { invoke, path, shell } from '@tauri-apps/api'
 import type { Child } from '@tauri-apps/api/shell'
-
 export let pyChildProcess: Child = null
 
 export async function startServer() {
     if (get(pyServerReady)) return console.info('Server already running')
-    const serverDebug = JSON.parse(localStorage.getItem('serverDebug')) ?? false
+    // const serverDebug = JSON.parse(localStorage.getItem('serverDebug')) ?? false
     // const availablePORT = (await invoke('get_tcp_port')) as number
     // pyServerPORT.set(availablePORT)
     console.info('starting felionpy server at port: ', get(pyServerPORT))
 
     pyServerReady.set(false)
     const pyfile = 'server'
-    const sendArgs = [pyfile, JSON.stringify({ port: get(pyServerPORT), debug: serverDebug })]
+    const sendArgs = [pyfile, JSON.stringify({ port: get(pyServerPORT), debug: get(serverDebug) })]
     const mainPyFile = await path.join(get(pythonscript), 'main.py')
 
     try {
