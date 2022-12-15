526405518b5858919e8627ccc9ec1e96c0e9bebf
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-12-15 21:28:17 +0100

    Update gh-release.ts

--- scripts/gh-release.ts
+++ scripts/gh-release.ts
@@ -1,11 +1,14 @@
-import { Octokit } from '@octokit/rest'
-import fs from 'fs'
-import { resolve, join } from 'path'
 import pkg from '../package.json'
+import { Octokit } from '@octokit/rest'
+import { resolve, join } from 'path'
+import fs from 'fs'
+
 console.log('working dir:', resolve('./'))
+
 const asset_dir = resolve('./src-tauri/target/release/bundle/msi')
 const asset_bin = `${pkg.name}_${pkg.version}_x64_en-US.msi.zip`
 const asset_sig = `${pkg.name}_${pkg.version}_x64_en-US.msi.zip.sig`
+
 const octokit = new Octokit({ auth: process.env.GH_FULL_CONTROL })
 
 const {
@@ -13,10 +16,13 @@ const {
 } = await octokit.rest.users.getAuthenticated()
 console.log('Hello, %s', login)
 
+const owner = 'aravindhnivas'
+const repo = pkg.name
+
 // Create the release
 const Release = await octokit.rest.repos.createRelease({
-    owner: 'aravindhnivas', // the owner of the repository
-    repo: pkg.name, // the name of the repository
+    owner, // the owner of the repository
+    repo, // the name of the repository
     tag_name: pkg.version, // the name of the release
     target_commitish: 'main', // the name of the branch
     name: `Release v${pkg.version}`, // the title of the release
@@ -25,8 +31,8 @@ const Release = await octokit.rest.repos.createRelease({
 
 // Upload an asset to the release
 const Sig_asset = await octokit.rest.repos.uploadReleaseAsset({
-    owner: 'aravindhnivas', // the owner of the repository
-    repo: pkg.name, // the name of the repository
+    owner, // the owner of the repository
+    repo, // the name of the repository
     release_id: Release.data.id, // the ID of the release
     name: asset_sig, // the name of the asset
     data: fs.readFileSync(join(asset_dir, asset_sig)), // the contents of the asset
@@ -36,19 +42,21 @@ const Sig_asset = await octokit.rest.repos.uploadReleaseAsset({
 console.log(`${Sig_asset.data.name}: ${Sig_asset.data.state}`)
 
 const data = await fs.promises.readFile(join(asset_dir, asset_bin))
+
 // Track the upload progress
 const progress = (uploadProgress) => {
     console.log(`Upload progress: ${uploadProgress.percent}%`)
 }
 
-// // Upload an asset to the release
-// const Bin_asset = await octokit.rest.repos.uploadReleaseAsset({
-//     owner: 'aravindhnivas', // the owner of the repository
-//     repo: pkg.name, // the name of the repository
-//     release_id: Release.data.id, // the ID of the release
-//     name: asset_bin, // the name of the asset
-//     data,
-//     contentType: 'application/zip', // the content type of the asset
-//     progress,
-// })
-// console.log(`${Bin_asset.data.name}: ${Bin_asset.data.state}`)
+// Upload an asset to the release
+const Bin_asset = await octokit.rest.repos.uploadReleaseAsset({
+    owner: 'aravindhnivas', // the owner of the repository
+    repo: pkg.name, // the name of the repository
+    release_id: Release.data.id, // the ID of the release
+    name: asset_bin, // the name of the asset
+    data,
+    contentType: 'application/zip', // the content type of the asset
+    progress,
+})
+
+console.log(`${Bin_asset.data.name}: ${Bin_asset.data.state}`)
