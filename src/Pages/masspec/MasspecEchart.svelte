834e8da2562046e1b480d9af224fad85736c90a8
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-12-19 19:42:13 +0100

    Update MasspecEchart.svelte

--- src/Pages/masspec/MasspecEchart.svelte
+++ src/Pages/masspec/MasspecEchart.svelte
@@ -1,24 +1,76 @@
 <script lang="ts">
     import * as echarts from 'echarts'
+
     export let edata
     export let id = window.getID()
 
     $: console.log({ edata })
-    $: if (edata) plotIt()
+    // $: if (edata) plotIt()
+
     const plotIt = (node = null) => {
-        const myChart = echarts.init(node ?? id)
-        const option = {
+        const graphDiv = document.getElementById(id)
+        const myChart = echarts.init(node ?? graphDiv)
+
+        const option: echarts.EChartsOption = {
+            // backgroundColor: 'whitesmoke',
+            // darkMode: true,
+            // title: {
+            //     text: 'Mass Spectrum',
+            //     left: '5%',
+            // },
+            tooltip: {
+                trigger: 'axis',
+            },
+            legend: {},
+            toolbox: {
+                show: true,
+                feature: {
+                    dataZoom: {
+                        yAxisIndex: 'none',
+                    },
+                    dataView: { readOnly: false },
+                    magicType: { type: ['line', 'bar'] },
+                    restore: {},
+                    saveAsImage: {},
+                },
+                right: 10,
+            },
+            dataZoom: [
+                {
+                    type: 'inside',
+                },
+            ],
             xAxis: {
                 type: 'value',
+                name: 'm/z',
+                nameLocation: 'middle',
+                nameTextStyle: {
+                    fontWeight: 'bolder',
+                    fontSize: 20,
+                    padding: 10,
+                },
                 data: [150, 230, 224, 218, 135, 147, 260],
             },
             yAxis: {
+                name: 'Counts',
+                nameLocation: 'middle',
+                nameTextStyle: {
+                    fontWeight: 'bolder',
+                    fontSize: 20,
+                    padding: 25,
+                },
                 type: 'value',
             },
             series: [
                 {
                     data: [150, 230, 224, 218, 135, 147, 260],
                     type: 'line',
+                    name: '1',
+                },
+                {
+                    data: [15, 230, 14, 260],
+                    type: 'line',
+                    name: '2',
                 },
             ],
         }
@@ -28,12 +80,13 @@
     }
 </script>
 
-<div {id} class="align echart_div">{id}</div>
+<button class="button is-link" on:click={() => plotIt()}>PlotIt</button>
+<div use:plotIt {id} class="align echart_div" />
 
 <style>
     .echart_div {
-        width: 1000px;
-        height: 500px;
+        width: 1100px;
+        height: 1000px;
         background-color: whitesmoke;
         color: black;
     }
