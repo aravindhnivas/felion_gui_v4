e4a244064263497c10c02d7a61d20a5f79fd4b10
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-12-21 14:48:15 +0100

    Update event_listeneres.ts

--- src/lib/event_listeneres.ts
+++ src/lib/event_listeneres.ts
@@ -1,12 +1,8 @@
 import { appWindow } from '@tauri-apps/api/window'
-import { confirm } from '@tauri-apps/api/dialog'
 import { stopServer } from '$lib/pyserver/felionpyServer'
 export const events_listeners = async () => {
     const unlisten_closeEvent = await appWindow.onCloseRequested(async (event) => {
-        if (!(await confirm('Are you sure to close the application?'))) {
-            event.preventDefault()
-        }
         await stopServer()
     })
-    return unlisten_closeEvent
+    return [unlisten_closeEvent]
 }
