96ad8a7bb9c39f4196af170b47e80f39fafce253
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-12-26 13:15:53 +0100

    Update OPORow.svelte

--- src/Pages/normline/widgets/preprocessing/OPORow.svelte
+++ src/Pages/normline/widgets/preprocessing/OPORow.svelte
@@ -30,9 +30,9 @@
     let OPOcalibFiles = []
 
     const update_opo_files = async (loc, files: string[]) => {
-        const dirs = await fs.readDir(OPOLocation)
+        const dirs = await fs.readDir(loc)
         OPOcalibFiles = dirs.map((f) => f.name).filter((file) => file.endsWith('.calibOPO'))
-        opofiles = await Promise.all(files.map(async (file) => await path.resolve(OPOLocation, file)))
+        opofiles = await Promise.all(files.map(async (file) => await path.resolve(loc, file)))
     }
 
     $: update_opo_files(OPOLocation, OPOfilesChecked)
