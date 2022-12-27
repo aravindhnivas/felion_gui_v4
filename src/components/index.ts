c967d68c1891dd8d0a23701dc6d08faf6639ac9e
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-12-27 13:36:29 +0100

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,54 +1,26 @@
-import Switch from './Switch.svelte'
-import Panel from './Panel.svelte'
-import Radio from './Radio.svelte'
-import SegBtn from './SegBtn.svelte'
-import TextSwitch from './TextSwitch.svelte'
-import Checkbox from './Checkbox.svelte'
-import Select from './Select.svelte'
-import FileBrowser from './common/FileBrowser.svelte'
-import Modal from './modal/Modal.svelte'
-import BrowseTextfield from './BrowseTextfield.svelte'
-import IconButton from './IconButton.svelte'
-import Textfield from '@smui/textfield'
+export { default as Switch } from './Switch.svelte'
+export { default as Panel } from './Panel.svelte'
+export { default as Radio } from './Radio.svelte'
+export { default as SegBtn } from './SegBtn.svelte'
+export { default as TextSwitch } from './TextSwitch.svelte'
+export { default as Checkbox } from './Checkbox.svelte'
+export { default as Select } from './Select.svelte'
+export { default as FileBrowser } from './common/FileBrowser.svelte'
+export { default as Modal } from './modal/Modal.svelte'
+export { default as BrowseTextfield } from './BrowseTextfield.svelte'
+export { default as IconButton } from './IconButton.svelte'
+export { default as VirtualCheckList } from './lists/VirtualCheckList.svelte'
+export { default as IconSwitch } from './IconSwitch.svelte'
+export { default as TextAndSelectOptsToggler } from './TextAndSelectOptsToggler.svelte'
+export { default as SModal } from './modal/SModal.svelte'
+export { default as Table } from './tables/Table.svelte'
+export { default as ModalTable } from './modal/ModalTable.svelte'
+export { default as FileReadAndLoad } from './fs/FileReadAndLoad.svelte'
+export { default as STable } from './tables/STable.svelte'
+export { default as ButtonBadge } from './ButtonBadge.svelte'
+export { default as SeparateWindow } from './misc/SeparateWindow.svelte'
+export { default as Notify } from './alert/Notify.svelte'
+export { default as QuickBrowser } from './quickDrawers/QuickBrowser.svelte'
+export { default as CheckList } from './lists/CheckList.svelte'
 
-import VirtualCheckList from './lists/VirtualCheckList.svelte'
-import IconSwitch from './IconSwitch.svelte'
-import TextAndSelectOptsToggler from './TextAndSelectOptsToggler.svelte'
-import SModal from './modal/SModal.svelte'
-import Table from './tables/Table.svelte'
-import ModalTable from './modal/ModalTable.svelte'
-import FileReadAndLoad from './fs/FileReadAndLoad.svelte'
-import STable from './tables/STable.svelte'
-import ButtonBadge from './ButtonBadge.svelte'
-import SeparateWindow from './misc/SeparateWindow.svelte'
-import Notify from './alert/Notify.svelte'
-import QuickBrowser from './quickDrawers/QuickBrowser.svelte'
-import CheckList from './lists/CheckList.svelte'
-
-export {
-    CheckList,
-    QuickBrowser,
-    Notify,
-    SeparateWindow,
-    ButtonBadge,
-    FileReadAndLoad,
-    Modal,
-    ModalTable,
-    STable,
-    Table,
-    SModal,
-    Panel,
-    Radio,
-    Switch,
-    Select,
-    SegBtn,
-    Checkbox,
-    Textfield,
-    TextSwitch,
-    IconSwitch,
-    IconButton,
-    FileBrowser,
-    BrowseTextfield,
-    VirtualCheckList,
-    TextAndSelectOptsToggler,
-}
+export { default as Textfield } from '@smui/textfield'
