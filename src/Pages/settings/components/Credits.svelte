e4bce94a86b7b36f7cf50d5703cd70cfb428a04d
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-12-27 16:39:34 +0100

    Create Credits.svelte

---
+++ src/Pages/settings/components/Credits.svelte
@@ -0,0 +1,102 @@
+<script>
+    import { currentTab } from '$lib/pyserver/stores'
+    import { ListsMultiline } from '$src/components'
+    const credits = [
+        {
+            primary: 'Tauri',
+            secondary: [
+                'Build an optimized, secure, and frontend-independent application for multi-platform deployment',
+            ],
+            link: '',
+            id: window.getID(),
+        },
+
+        {
+            primary: 'Svelte',
+            secondary: ['CYBERNETICALLY ENHANCED WEB APPS'],
+            id: window.getID(),
+        },
+
+        {
+            primary: 'Vite',
+            secondary: ['Next Generation Frontend Tooling'],
+            id: window.getID(),
+        },
+
+        {
+            primary: 'Bulma',
+            secondary: ['the modern CSS framework that just works'],
+            link: 'https://bulma.io/',
+            id: window.getID(),
+        },
+
+        {
+            primary: 'Google fonts',
+            secondary: ['Material Symbols and fonts'],
+            link: 'https://fonts.google.com/icons',
+            id: window.getID(),
+        },
+
+        {
+            primary: 'Plotly.js',
+            secondary: ['JavaScript Open Source Graphing Library'],
+            link: 'https://plotly.com/javascript/',
+            id: window.getID(),
+        },
+
+        {
+            primary: 'SMUI',
+            secondary: ['Material UI components for Svelte apps'],
+            id: window.getID(),
+        },
+
+        {
+            primary: 'Lodash',
+            secondary: ['A modern JavaScript utility library delivering modularity, performance & extras'],
+            link: 'https://lodash.com/',
+            id: window.getID(),
+        },
+
+        {
+            primary: 'Lordicons',
+            secondary: ['Animated icons by Lordicon.com'],
+            id: window.getID(),
+        },
+
+        {
+            primary: 'Animate.css',
+            secondary: ['Just-add-water CSS animations'],
+            id: window.getID(),
+        },
+
+        {
+            primary: 'Hover.css',
+            secondary: ['A collection of CSS3 powered hover effects'],
+            link: 'https://github.com/IanLunn/Hover',
+            id: window.getID(),
+        },
+
+        {
+            primary: 'Cooltipz.css',
+            secondary: ['Cool customisable tooltips made from pure CSS'],
+            link: 'https://github.com/jackdomleo7/Cooltipz.css',
+            id: window.getID(),
+        },
+    ]
+</script>
+
+<div class="animate__animated animate__fadeIn" class:hide={$currentTab !== 'Credits'}>
+    <h1>Credits</h1>
+    <div class="credits__div">
+        <ListsMultiline lists={credits} />
+    </div>
+</div>
+
+<style>
+    .credits__div {
+        display: grid;
+        user-select: text;
+        max-height: 75vh;
+        overflow: auto;
+    }
+</style>
