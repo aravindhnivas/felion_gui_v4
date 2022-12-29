05186adbbc4739a9243f9725c2cb457bcc6574bb
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-12-29 15:13:19 +0100

    Update felionpy-build.ts

--- scripts/felionpy-build.ts
+++ scripts/felionpy-build.ts
@@ -1,18 +1,21 @@
 import { spawn } from 'child_process'
-import path from "path"
+import path from 'path'
 
-const cwd = path.resolve("./")
+const cwd = path.resolve('./')
 
-const resource = path.join(cwd, "src-tauri/resources")
-const icon = path.join(cwd, "src-tauri/icons/icon.ico")
-const pyfiles = path.join(resource, "python_files")
-const hooks = path.join(pyfiles, "hooks")
+const resource = path.join(cwd, 'src-tauri/resources')
+const icon = path.join(cwd, 'src-tauri/icons/icon.ico')
+const pyfiles = path.join(cwd, 'src-python')
+const hooks = path.join(pyfiles, 'hooks')
 const mainfile = path.join(pyfiles, 'main.py')
-const args = `--noconfirm --onedir --console --icon ${icon} --name felionpy --debug noarchive --noupx --additional-hooks-dir ${hooks} --hidden-import felionlib --paths ${pyfiles} --distpath ${resource} ${mainfile}`.split(' ')
+const args =
+    `--noconfirm --onedir --console --icon ${icon} --name felionpy --debug noarchive --noupx --additional-hooks-dir ${hooks} --hidden-import felionlib --paths ${pyfiles} --distpath ${resource} ${mainfile}`.split(
+        ' '
+    )
 console.log(args)
 
 const py = spawn('pyinstaller', args)
-py.stdout.on('data', (data) => console.log(data.toString("utf8")))
-py.stderr.on('data', (data) => console.log(data.toString("utf8")))
+py.stdout.on('data', (data) => console.log(data.toString('utf8')))
+py.stderr.on('data', (data) => console.log(data.toString('utf8')))
 py.on('close', () => console.log('closed'))
 py.on('error', (err) => console.log('error occured', err))
