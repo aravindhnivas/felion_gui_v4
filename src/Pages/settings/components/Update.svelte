5464d1e84735ab9a07d6bbc8905b994a65e1bc58
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-01-10 15:40:54 +0100

    UNZIP downloaded contents

--- src/Pages/settings/components/Update.svelte
+++ src/Pages/settings/components/Update.svelte
@@ -107,12 +107,13 @@
     async function downloadZIP(url, filename) {
         try {
             output = [{ value: 'downloading assets...', type: 'warning' }, ...output]
-            output = [{ value: url, type: 'warning' }, ...output]
-            const response = await http.fetch(url, {
+            const URL_to_download = downloadoverrideURL ? $downloadURL : url
+            output = [{ value: URL_to_download, type: 'warning' }, ...output]
+            const response = await http.fetch(URL_to_download, {
                 method: 'GET',
                 responseType: http.ResponseType.Binary,
             })
-            console.log(response)
+            // console.log(response)
             if (!response.ok) {
                 output = [{ value: `Status ${response.status} : Invalid URL`, type: 'danger' }, ...output]
                 return
@@ -120,10 +121,45 @@
             output = [{ value: `assets downloaded`, type: 'success' }, ...output]
             await fs.writeBinaryFile(filename, response.data as Uint8Array, { dir: fs.BaseDirectory.AppLocalData })
             output = [{ value: `assets saved`, type: 'success' }, ...output]
+            await unZIP(filename)
         } catch (error) {
             output = [{ value: `error occure while downloading assets`, type: 'danger' }, ...output]
             window.handleError(error)
         }
+    }
+    async function unZIP(filename) {
+        const filepath = await path.appLocalDataDir()
+        const cmd = new shell.Command(`unzip-${await platform()}`, [
+            'Expand-Archive',
+            '-Path',
+            `${await path.join(filepath, filename)}`,
+            '-DestinationPath',
+            `${filepath}`,
+        ])
+
+        let err: string
+        const child = await cmd.spawn()
+
+        output = [{ value: `unzip PID: ${child.pid}`, type: 'info' }, ...output]
+        cmd.on('close', () => {
+            output = [{ value: 'UNZIP closed', type: 'info' }, ...output]
+            output = [{ value: err ? 'failed to UNZIP' : 'UNZIP success', type: err ? 'danger' : 'success' }, ...output]
+        })
+
+        cmd.on('error', (error) => {
+            err = error
+            output = [{ value: 'Error whiile UNZIPing assets', type: 'danger' }, ...output]
+            output = [{ value: JSON.stringify(error), type: 'danger' }, ...output]
+        })
+
+        cmd.stderr.on('data', (stderr) => {
+            err = stderr
+            output = [{ value: JSON.stringify(stderr), type: 'danger' }, ...output]
+        })
+
+        cmd.stdout.on('data', (stdout) => {
+            output = [{ value: JSON.stringify(stdout), type: 'info' }, ...output]
+        })
     }
 
     const check_assets_update = async () => {
@@ -157,7 +193,10 @@
         }
     }
 
-    let downloadURL = 'https://github.com/aravindhnivas/felionpy/releases/download/v0.0.11/felionpy-win64.zip'
+    const downloadURL = persistentWritable(
+        'download_URL_test',
+        'https://github.com/aravindhnivas/felionpy/archive/refs/tags/v0.0.11.zip'
+    )
     const download_assets = async () => {
         if (!assets_version_available) {
             output = [{ value: 'Check for assets update first.', type: 'warning' }, ...output]
@@ -177,6 +216,7 @@
         if (!(await confirm('Download anyway', { title: 'Download not required' }))) return
         await downloadZIP(URL, asset_name)
     }
+    let downloadoverrideURL = false
 </script>
 
 <div class="align animate__animated animate__fadeIn" class:hide={$currentTab !== 'Update'}>
@@ -221,7 +261,8 @@
     <hr />
     <div class="align">
         {#if import.meta.env.DEV}
-            <Textfield bind:value={downloadURL} label="download-URL" style="width: 100%" />
+            <Textfield bind:value={$downloadURL} label="download-URL" style="width: 100%" />
+            <Switch bind:selected={downloadoverrideURL} label="override URL" />
         {/if}
         <span class="tag is-warning">assets download</span>
         <button class="button is-link" on:click={check_assets_update}>Check assets update</button>
