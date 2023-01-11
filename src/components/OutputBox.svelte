b29651a48795198f626019552bdb441f52fa3bec
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-01-11 23:19:42 +0100

    Update OutputBox.svelte

--- src/components/OutputBox.svelte
+++ src/components/OutputBox.svelte
@@ -2,20 +2,32 @@
     export let output: OutputBoxtype[] = []
     export let heading: string = ''
     export let style: string = ''
+    export let testMode: boolean = false
     let className: string = ''
     export { className as class }
 </script>
 
 <div class="console-box align box {className}" {style}>
-    <span class="tag">{heading}</span>
-    <button
-        class="button is-danger ml-auto"
-        on:click={() => {
-            output = []
-        }}>Clear</button
-    >
+    <span class="tag mr-auto">{heading}</span>
 
-    <hr />
+    <div class="ml-auto">
+        {#if testMode}
+            <button
+                class="button is-link ml-auto"
+                on:click={() => {
+                    output = [{ value: 'Testing', type: 'warning' }, ...output]
+                }}>Add</button
+            >
+        {/if}
+
+        <button
+            class="button is-danger"
+            on:click={() => {
+                output = []
+            }}>Clear</button
+        >
+    </div>
+    <hr style="width: 100%;" />
     {#each output as info (info)}
         {#if info.value}
             <span class="has-text-{info.type}" style="width: 100%;">>> {info.value}</span>
