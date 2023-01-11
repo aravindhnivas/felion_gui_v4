b095a1ec4ff96ab35e0b93c1dff34d60e8cfe0c5
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-01-11 17:12:40 +0100

    Update download.rs

--- src-tauri/src/download.rs
+++ src-tauri/src/download.rs
@@ -1,5 +1,8 @@
 use error_chain::error_chain;
-// use futures::stream::{Stream, StreamExt};
+// use futures::stream::Stream;
+use futures_util::StreamExt;
+use std::io::Write;
+
 error_chain! {
     foreign_links {
         Io(std::io::Error);
@@ -14,6 +17,33 @@ pub async fn download_url_main(url: &str, file_name: &str) -> Result<()> {
     if !response.status().is_success() {
         return Err(format!("Failed to download file: {}", response.status()).into());
     }
-    std::fs::write(file_name, response.bytes().await?)?;
+
+    let total_size = response
+        .headers()
+        .get("content-length")
+        .and_then(|v| v.to_str().ok())
+        .and_then(|v| v.parse::<u64>().ok())
+        .unwrap_or(0);
+    let mut bytes_downloaded = 0;
+
+    println!("total_size : {}", total_size);
+
+    if total_size == 0 {
+        return Err(format!("Failed to download file: {}", response.status()).into());
+    }
+    let mut stream = response.bytes_stream();
+    let mut file = std::fs::File::create(file_name)?;
+    while let Some(Ok(chunk)) = stream.next().await {
+        bytes_downloaded += chunk.len() as u64;
+        let progress = (bytes_downloaded * 100) / total_size;
+        println!(
+            "Downloaded {}/{} bytes ({}%)",
+            bytes_downloaded, total_size, progress
+        );
+        file.write_all(&chunk)?;
+    }
+
+    println!("File downloaded and saved: {}", file_name);
+    // std::fs::write(file_name, response.bytes().await?)?;
     Ok(())
 }
