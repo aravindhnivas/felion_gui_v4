96ee46c825db05319b593653c6b5331042de28fa
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-01-11 15:54:44 +0100

    Update download.rs

--- src-tauri/src/download.rs
+++ src-tauri/src/download.rs
@@ -1,5 +1,5 @@
 use error_chain::error_chain;
-
+use futures::stream::{Stream, StreamExt};
 error_chain! {
     foreign_links {
         Io(std::io::Error);
@@ -14,7 +14,6 @@ pub async fn download_url_main(url: &str, file_name: &str) -> Result<()> {
     if !response.status().is_success() {
         return Err(format!("Failed to download file: {}", response.status()).into());
     }
-    println!("File downloaded and saved to {}", file_name);
     std::fs::write(file_name, response.bytes().await?)?;
     Ok(())
 }
