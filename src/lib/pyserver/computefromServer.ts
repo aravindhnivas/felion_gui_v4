f1b103cbf22016dc3aef7ded60ad74f59d2c2d40
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-01-12 16:18:14 +0100

    changing TEMP dir

--- src/lib/pyserver/computefromServer.ts
+++ src/lib/pyserver/computefromServer.ts
@@ -14,7 +14,7 @@ export default async function ({ pyfile, args, target, general }: Type): Promise
         if (!general) {
             target?.classList.add('is-loading')
             const filename = pyfile.split('.').at(-1) + '_data.json'
-            const outputFile = await path.join(window.tempdirPath, 'FELion_GUI3', filename)
+            const outputFile = await path.join(window.tempdirPath, 'com.felion.app', filename)
             if (await fs.exists(outputFile)) {
                 const [_err] = await oO(fs.removeFile(outputFile))
                 if (_err) console.error(_err)
