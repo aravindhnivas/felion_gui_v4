3a9e81361f16954f87371a1634fc0d565f2679f3
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-01-13 16:49:10 +0100

    Update assets-status.ts

--- src/Pages/settings/utils/assets-status.ts
+++ src/Pages/settings/utils/assets-status.ts
@@ -1,6 +1,5 @@
-import { downloadoverrideURL, python_asset_ready, outputbox, asset_download_required } from './stores'
+import { downloadoverrideURL, python_asset_ready, outputbox } from './stores'
 import { check_assets_update, download_assets, unZIP } from './download-assets'
-import { felionlibVersion } from '$src/lib/pyserver/stores'
 
 export const check_felionpy_assets_status = async () => {
     try {
@@ -9,19 +8,14 @@ export const check_felionpy_assets_status = async () => {
             python_asset_ready.set(true)
             return
         }
-        console.warn('felionpy asset REQUIRED!!')
 
+        console.warn('felionpy asset REQUIRED!!')
         if (!(await dialog.confirm('Python assets are missing. Press OK to download.'))) return
 
         await auto_download_and_install_assets()
     } catch (error) {
         outputbox.error(error)
     }
-}
-
-export const check_whether_asset_update_required = () => {
-    if (get(felionlibVersion) >= import.meta.env.VITE_FELIONPY_MIN_VERSION) return
-    asset_download_required.set(true)
 }
 
 export const auto_download_and_install_assets = async () => {
