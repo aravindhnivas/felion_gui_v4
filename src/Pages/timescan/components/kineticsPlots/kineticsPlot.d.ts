031f8e84d64d0dd4ebfc333873192677502c9b9b
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-02-28 21:05:30 +0100

    Create kineticsPlot.d.ts

---
+++ src/Pages/timescan/components/kineticsPlots/kineticsPlot.d.ts
@@ -0,0 +1,31 @@
+export as namespace KineticsPlot
+
+export interface RateConstantProcessed {
+    [key: string]: {
+        [key: string]: {
+            val: number[]
+            std: number[]
+            mean: string
+            weighted_mean: string
+            number_densities: {
+                val: number[]
+                std: number[]
+            }
+        }
+    }
+}
+
+export interface RateConstantFitted {
+    [key: string]: {
+        [key: string]: {
+            val: number[]
+            std: number[]
+            slope: string
+            intercept: string
+            number_densities: {
+                val: number[]
+                std: number[]
+            }
+        }
+    }
+}
