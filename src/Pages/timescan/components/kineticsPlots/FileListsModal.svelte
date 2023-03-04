41775c229af8da32e391c190a5289040a98147c6
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-03-04 15:45:31 +0100

    Update FileListsModal.svelte

--- src/Pages/timescan/components/kineticsPlots/FileListsModal.svelte
+++ src/Pages/timescan/components/kineticsPlots/FileListsModal.svelte
@@ -3,6 +3,7 @@
     export let active: boolean = false
     export let fileCollections: { name: string; selected: boolean }[] = []
     let toggle_selections = false
+    $: file_length = fileCollections.filter((f) => f.selected).length
 </script>
 
 {#if active}
@@ -12,6 +13,13 @@
         </svelte:fragment>
 
         <svelte:fragment slot="footerbtn">
+            <span class="tag is-warning" class:is-danger={file_length === 0}>
+                {#if file_length === 0}
+                    No files selected
+                {:else}
+                    {file_length} files selected
+                {/if}
+            </span>
             <button
                 class="button is-link"
                 on:click={() => {
