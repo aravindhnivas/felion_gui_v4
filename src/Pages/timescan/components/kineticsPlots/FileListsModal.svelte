2edec1894ea9140d7f7e03d94c40ae86fcdf2114
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-03-04 16:28:08 +0100

    Update FileListsModal.svelte

--- src/Pages/timescan/components/kineticsPlots/FileListsModal.svelte
+++ src/Pages/timescan/components/kineticsPlots/FileListsModal.svelte
@@ -1,15 +1,29 @@
 <script lang="ts">
-    import { SegBtn, Modal } from '$src/components'
+    import { Modal, Textfield } from '$src/components'
     export let active: boolean = false
     export let fileCollections: { name: string; selected: boolean }[] = []
     let toggle_selections = false
     $: file_length = fileCollections.filter((f) => f.selected).length
+    let search_name = ''
 </script>
 
 {#if active}
     <Modal bind:active title="File Lists: Left-click to toggle selection">
         <svelte:fragment slot="content">
-            <SegBtn bind:choices={fileCollections} />
+            <!-- <SegBtn bind:choices={fileCollections} /> -->
+
+            <div class="flex flex-wrap gap-2">
+                <Textfield bind:value={search_name} label="search filenames" style="width:100%" />
+                {#each fileCollections as { name, selected } (name)}
+                    {@const hide = name.toLowerCase().indexOf(search_name.toLowerCase()) === -1}
+                    <span class="tag is-warning" class:is-danger={!selected} class:hide>
+                        {name}
+                        <button class="delete is-small" on:click={() => (selected = !selected)} />
+                    </span>
+                {:else}
+                    <span>No files</span>
+                {/each}
+            </div>
         </svelte:fragment>
 
         <svelte:fragment slot="footerbtn">
