0370f25534a15fc6f9a262069d1ad55f551cbb6f
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-03-06 15:21:35 +0100

    Update EnergyConversion.svelte

--- src/Pages/misc/EnergyConversion.svelte
+++ src/Pages/misc/EnergyConversion.svelte
@@ -2,17 +2,17 @@
     import { Textfield } from '$src/components'
     import { boltzmanConstant, PlanksConstant, electricCharge, SpeedOfLight } from '$src/js/constants'
 
-    const fixedDigit = persistentWritable('energyConversion_fixedDigit', 3)
+    const fixedDigit = persistentWritable('energyConversion_fixedDigit', 2)
 
-    $: hz = Number((1e12).toExponential($fixedDigit))
-    $: eV = Number((PlanksConstant / electricCharge) * hz).toFixed($fixedDigit)
-    $: kelvin = Number((PlanksConstant / boltzmanConstant) * hz).toFixed($fixedDigit)
+    $: hz = (1e12).toExponential($fixedDigit)
+    $: eV = Number((PlanksConstant / electricCharge) * Number(hz)).toExponential($fixedDigit)
+    $: kelvin = Number((PlanksConstant / boltzmanConstant) * Number(hz)).toFixed($fixedDigit)
 
-    $: cm_1 = Number(hz / (SpeedOfLight * 1e2)).toFixed($fixedDigit)
-    $: um = Number((SpeedOfLight / hz) * 1e6).toFixed($fixedDigit)
-    $: ghz = Number(hz * 1e-9).toFixed($fixedDigit)
-    $: nm = Number((SpeedOfLight / hz) * 1e9).toFixed($fixedDigit)
-    $: J = Number(PlanksConstant * hz).toExponential($fixedDigit)
+    $: cm_1 = Number(Number(hz) / (SpeedOfLight * 1e2)).toFixed($fixedDigit)
+    $: um = Number((SpeedOfLight / Number(hz)) * 1e6).toFixed($fixedDigit)
+    $: ghz = Number(Number(hz) * 1e-9).toFixed($fixedDigit)
+    $: nm = Number((SpeedOfLight / Number(hz)) * 1e9).toExponential($fixedDigit)
+    $: J = Number(PlanksConstant * Number(hz)).toExponential($fixedDigit)
 </script>
 
 <div class="box">
@@ -23,22 +23,44 @@
     <div class="scroll">
         <div class="align">
             <Textfield bind:value={hz} label="Hz" />
-            <Textfield bind:value={ghz} on:change={() => (hz = Number(ghz) * 1e9)} label="GHz" />
-            <Textfield bind:value={um} on:change={() => (hz = (SpeedOfLight / Number(um)) * 1e6)} label="Micron" />
-            <Textfield bind:value={cm_1} on:change={() => (hz = Number(cm_1) * SpeedOfLight * 1e2)} label="cm-1" />
+            <Textfield
+                bind:value={ghz}
+                on:change={() => (hz = Number(Number(ghz) * 1e9).toExponential($fixedDigit))}
+                label="GHz"
+            />
+            <Textfield
+                bind:value={um}
+                on:change={() => (hz = Number((SpeedOfLight / Number(um)) * 1e6).toExponential($fixedDigit))}
+                label="Micron"
+            />
+            <Textfield
+                bind:value={cm_1}
+                on:change={() => (hz = Number(Number(cm_1) * SpeedOfLight * 1e2).toExponential($fixedDigit))}
+                label="cm-1"
+            />
             <Textfield
                 bind:value={kelvin}
-                on:change={() => (hz = (boltzmanConstant / PlanksConstant) * Number(kelvin))}
+                on:change={() =>
+                    (hz = Number((boltzmanConstant / PlanksConstant) * Number(kelvin)).toExponential($fixedDigit))}
                 label="Kelvin"
             />
             <Textfield
                 bind:value={eV}
-                on:change={() => (hz = (electricCharge / PlanksConstant) * Number(eV))}
+                on:change={() =>
+                    (hz = Number((electricCharge / PlanksConstant) * Number(eV)).toExponential($fixedDigit))}
                 label="eV"
             />
-            <Textfield bind:value={J} on:change={() => (hz = Number(J) / PlanksConstant)} label="Joule" />
+            <Textfield
+                bind:value={J}
+                on:change={() => (hz = Number(Number(J) / PlanksConstant).toExponential($fixedDigit))}
+                label="Joule"
+            />
 
-            <Textfield bind:value={nm} on:change={() => (hz = (SpeedOfLight / Number(nm)) * 1e9)} label="nm" />
+            <Textfield
+                bind:value={nm}
+                on:change={() => (hz = Number((SpeedOfLight / Number(nm)) * 1e9).toExponential($fixedDigit))}
+                label="nm"
+            />
         </div>
 
         <hr />
