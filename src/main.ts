4bc2bd648607e30db9c031785870d2a3dcc8768b
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-03-06 22:44:02 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import App from './App.svelte'
+import 'uno.css'
 import './js/functions'
 import './App.scss'
 import './misc'
-import 'uno.css'
 
 const app = new App({
     target: document.getElementById('app'),
