7f3a02cd7a484beb87e6a319df059cd921515a91
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-03-14 11:36:47 +0100

    Update Footer_label.svelte

--- src/layout/main/footer_utils/Footer_label.svelte
+++ src/layout/main/footer_utils/Footer_label.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
     import { pyServerReady } from '$lib/pyserver/stores'
-    import { start_and_check_felionpy_with_toast, stopServer } from '$src/lib/pyserver/felionpyServer'
-    import { check_felionpy_assets_status } from '$src/Pages/settings/utils/assets-status'
     import {
         python_asset_ready,
         installing_python_assets,
         python_asset_ready_to_install,
     } from '$src/Pages/settings/utils/stores'
-    import { activePage } from '$src/sveltewritables'
+    import { start_and_check_felionpy_with_toast, stopServer } from '$src/lib/pyserver/felionpyServer'
+    import { check_felionpy_assets_status } from '$src/Pages/settings/utils/assets-status'
 </script>
 
 <div class="navbar-item py-0" style="gap: 0.5em;">
