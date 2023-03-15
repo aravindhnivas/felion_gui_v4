ee9a365d9eee86fdc1d717f20cad46afcc1bd3e4
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-03-15 13:01:34 +0100

    Update EntryMode.svelte

--- src/Pages/masspec/components/db/EntryMode.svelte
+++ src/Pages/masspec/components/db/EntryMode.svelte
@@ -9,20 +9,30 @@
     <h3 style="width: 100%;">Required entires</h3>
 
     <Select label="filename" options={filenames} bind:value={$entry_values.filename} />
-    <Textfield label="temperature (K)" bind:value={$entry_values.temperature} />
+    <Textfield variant="outlined" label="Enter temperature (K)" bind:value={$entry_values.temperature} />
 
-    <Textfield label="precursor name" bind:value={$entry_values.precursor} />
-    <Textfield label="pressure (mBar)" bind:value={$entry_values.pressure} />
-    <Textfield label="IE (eV)" bind:value={$entry_values.IE} />
+    <Textfield variant="outlined" label="Enter precursor name" bind:value={$entry_values.precursor} />
+    <Textfield variant="outlined" label="Enter pressure (mBar)" bind:value={$entry_values.pressure} />
+    <Textfield variant="outlined" label="Enter IE (eV)" bind:value={$entry_values.IE} />
 
-    <Select label="Source type" bind:value={$entry_values.source} options={['storage', 'non-storage']} />
+    <Select label="Select ion-source type" bind:value={$entry_values.source} options={['storage', 'non-storage']} />
 
     <hr />
 
-    <h3 style="width: 100%;">Optional entires (comma separated)</h3>
+    <h3 style="width: 100%;">Optional entires</h3>
 
-    <Textfield style="width: 100%" label="keywords" bind:value={$entry_values.keywords} />
-    <Textfield style="width: 100%" label="notes" bind:value={$entry_values.notes} />
+    <Textfield
+        variant="outlined"
+        style="width: 100%"
+        label="Enter keywords (for instance experiment's target molecule, FELIX shift, etc.)"
+        bind:value={$entry_values.keywords}
+    />
+    <Textfield
+        variant="outlined"
+        style="width: 100%"
+        label="Enter notes (for instance who performed the experiments or testing after changes in a instrument, etc. )"
+        bind:value={$entry_values.notes}
+    />
 
     <button class="button is-danger ml-auto mt-5" on:click={$entry_values.reset}>Reset</button>
 </div>
